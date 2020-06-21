1ead986090e6e17bd8090f392a8c93a4179b64ea
jonathan garcia <jonathan.g@geopagos.com>
2020-06-21 19:31:57 -0300

    Initial commit

---
+++ MovieApp/MovieApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MovieApp
+//
+//  Created by Jonathan Garcia on 21/06/2020.
+//  Copyright © 2020 Jonathan Garcia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MovieApp/MovieApp/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  MovieApp
+//
+//  Created by Jonathan Garcia on 21/06/2020.
+//  Copyright © 2020 Jonathan Garcia. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
